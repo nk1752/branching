4e5effe160fa6e339bc0a9a45f9b678c412bd9ee
nk1752 <71557302+nk1752@users.noreply.github.com>
2024-06-10 08:52:33 -0500

    test curl

--- test.sh
+++ test.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
 echo "Hello bash shript"
- 
+
+if [ ${{ github.event_name }} == "push" ]; then
+  echo "This is a push event"
+elif [[ ${{ github.event_name }} == "pull_request" ]]; then
+  echo "This is a pull request event"
+else
+  echo "This is an unknown event"
+  # get current temperture
+  curl  -X GET "https://api.openweathermap.org/data/2.5/weather?q=Hanoi&appid=8f \
+        -u 8f \
+        -H "Content-Type: application/json"    
+fi
  
